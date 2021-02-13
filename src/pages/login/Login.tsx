e0f8558af73393ec133370bf4d3476dde576767d
Egemen Ceylan <egemen.ceylan1@gmail.com>
2021-02-13 20:03:51 +0300

    Postcss files are added

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "../login/Login.module.css";
+
+function Login() {
+  return (
+    <div className={styles.container}>
+      <p>Login Page</p>
+    </div>
+  );
+}
+
+export default Login;
